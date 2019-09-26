9a2a6f96f780f50babf90946ebc8c66a08957699
SSS <staster86@gmail.com>
2019-09-26 22:13:41 +0400

    Методы пошли

---
+++ Head_first/test_drive.rb
@@ -0,0 +1,14 @@
+def accelerate
+  puts "Визг колёс!"
+  puts "Машина стремительно разгоняется!"
+end
+
+def sound_horn
+  puts "Нажимаем кнопку сигнала"
+  puts "Beep, Beep!!!"
+end
+
+def use_headlights (brightness)
+  puts "Включаем свет #{brightness} фары"
+  puts "Остерегайтесь оленей!"
+end
