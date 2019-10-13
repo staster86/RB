dc315d9957c5db864c21cec687f0ebe664ee0be8
SSS <staster86@gmail.com>
2019-10-13 22:31:31 +0400

    Задание решено

---
+++ opennet/deaf_granny.rb
@@ -0,0 +1,17 @@
+#Глухая бабуля
+
+voice = ""
+
+while voice != 'ПОКА ПОКА ПОКА'
+
+  puts "Скажите что-нибудь бабуле, чтобы она Вас услышала!"
+  voice = gets.chomp
+  year = rand(1930..1950)
+
+  if voice != voice.upcase
+    puts "АСЬ ?! ВНУЧЁК ГОВОРИ ПОГРОМЧЕ!"
+  elsif voice == voice.upcase && voice != 'ПОКА ПОКА ПОКА'
+    puts "НЕТ, НИ РАЗУ С #{year} ГОДА!"
+  end
+
+end
