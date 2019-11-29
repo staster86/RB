24667153244ca58ddbed762b21c2b43d4bf3a303
ST <staster86@gmail.com>
2019-11-29 17:17:45 +0400

    Закрепление материала про массивы

--- ruby_way/array.rb
+++ ruby_way/array.rb
@@ -1,1 +1,27 @@
+#Programms to-do list
+puts "Let's start topping up the to-do list" #Давайте начнем пополнять список дел
+puts "Go? (y/n)" #Поехали?
+start = gets.chomp
+
+if start.downcase == 'y'
+  stop = "y"
+  affairs = []
+  while stop.downcase == "y"
+    num_item = 1
+    puts "Enter a case, preferably short and clear:"
+    enter = gets.chomp.capitalize
+    affairs.push(enter)
+    puts "Current to-do list"
+    affairs.each do |item|
+      puts "#{num_item}.#{item}"
+      num_item = num_item + 1
+    end
+    puts "Waiting to continue? (y/n)"
+    stop = gets.chomp
+  end
+else
+  puts "Error command!"
+end
+
+
 
