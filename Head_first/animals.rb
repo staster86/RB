614a76e25ed08c89d18f41726ccdaec947409206
ST <staster86@gmail.com>
2019-12-01 21:15:04 +0400

    Не большой порядок навёл

--- Head_first/animals.rb
+++ Head_first/animals.rb
@@ -2,15 +2,15 @@
 class Animal
   attr_reader :name, :age #Определение методов чтения атрибутов name и age
 
-  def name=(value) #Метод записи атрибута для @name
-    if value == "" #Проверка на пустрое значение атрибута
-      raise "Name can't be black!"
+  def name=(value) #Метод записи атрибута для @name, при вызове метода у экземпляра класса можно передать значение
+    if value == "" #Далее проверка на пустрое значение атрибута
+      raise "Name can't be black!"#Остановка с указание ошибки
     end
     @name = value #Переменная @name класса Bird
   end
 
   def age=(value) #Метод записи атрибута для @age
-    if value < 0 #Проверка возраста
+    if value < 0 #Проверка возраста на отрицательное значение
       raise "An age of #{value} isn't valid!"
     end
     @age = value
@@ -46,6 +46,7 @@ class Bird < Animal
 end
 
 #Создание объектов из класса и использование методов и атрибутов
+=begin
 kisen = Bird.new
 kisen.name = "Kesha"
 kisen.age = 1
@@ -59,3 +60,4 @@ bobik.age = 3
 bobik.report_age
 bobik.talk
 bobik.move("yard")
+=end
