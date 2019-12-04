5952f075f124ff9953383ee8b2dbdf70489d31ee
ST <staster86@gmail.com>
2019-12-04 17:54:12 +0400

    Класс дописан

--- Head_first/employee.rb
+++ Head_first/employee.rb
@@ -1,6 +1,10 @@
 class Employee
 
-  attr_reader :name, :salary
+  attr_reader :name
+
+  def initialize(name = "Anonymous")
+    self.name = name # Метод self вызывает метод name именно этого экземпляра
+  end
 
   def name=(name)
     if name == ""
@@ -8,6 +12,17 @@ class Employee
     end
     @name = name
   end
+
+  def print_name
+    puts "Name: #{name}" # Тоже самое что self.name
+  end
+
+end
+
+# Оплата по ставке
+class SalaryEmployee < Employee
+
+  attr_reader :salary
 
   def salary=(salary)
     if salary < 0
@@ -20,14 +35,48 @@ class Employee
   # Передача параметров при создании
   # Если методу не переданны параметры то подставляются умолчания
   # Метод инициализации переменных экземпляра класса
-  def initialize(name = "Anonymous",salary = 0.0)
-    @name = name
-    @salary = salary
+  def initialize(name = "Anonimous", salary = 0.0)
+    super(name)
+    self.salary = salary
   end
 
   def print_pay_stub
-    puts "Name: #{@name}"
-    pay_for_period = (@salary / 365.0) * 14
+    print_name
+    pay_for_period = (self.salary / 365.0) * 14
+    formatted_pay = format("%.2f", pay_for_period)
+    puts "Pay This Period: $#{formatted_pay}"
+  end
+
+end
+
+# Почасовая оплата труда
+class HourlyEmployee < Employee
+  # Почасовая зарплата, часов в неделю
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0.0
+      raise "Negative salary!!!"
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0.0
+      raise "Negative business works!"
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  def initialize(name = "Anonimous", hourly_wage = 0, hours_per_week = 0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
     formatted_pay = format("%.2f", pay_for_period)
     puts "Pay This Period: $#{formatted_pay}"
   end
