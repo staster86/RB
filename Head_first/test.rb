2fa17b4c8691a74404def6dc1138f8ea6aa15d34
ST <staster86@gmail.com>
2019-12-04 21:14:27 +0400

    Переместил метод info в субкласс

--- Head_first/test.rb
+++ Head_first/test.rb
@@ -4,11 +4,6 @@ class Boat
     @name = name
   end
 
-  def info
-    puts "Name: #{@name}"
-    puts "Motor type: #{@motor_type}"
-  end
-
 end
 
 class PowerBoat < Boat
@@ -16,6 +11,11 @@ class PowerBoat < Boat
   def initialize(name, motor_type)
     super(name)
     @motor_type = motor_type
+  end
+
+  def info
+    puts "Name: #{@name}"
+    puts "Motor type: #{@motor_type}"
   end
 
 end
