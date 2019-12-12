a5b937b125caf73b12ad77b4167d6a6c1c63db85
ST <staster86@gmail.com>
2019-12-12 14:47:34 +0400

    Хэши и значения по умолчанию, наложение имён и хэш с объектом и ссылкой на него.

--- Head_first/celestial_body.rb
+++ Head_first/celestial_body.rb
@@ -1,1 +1,17 @@
+class CelestiaBody
+  attr_accessor :name, :type
+end
 
+# Ключу хэша присваиваем объект типа CelestiaBody
+# Ключ передаётся так bodies['Something']
+# bodies['Something'].name = 'Something'
+# ['Something'] это ключ = key
+bodies = Hash.new do |hash, key| # = {}, 'Something'
+  body = CelestiaBody.new # Передаём ссылку на объект
+  body.type = 'planet' # Устанавливаем значение по умолчанию для параметра :type
+  hash[key] = body # {'Mars' => #<CelestiaBody:0x000000000553d4c8 @type="planet">}
+  body
+end
+
+bodies['Mars'] # {'Mars' => #<CelestiaBody:0x000000000553d4c8 @type="planet">}
+bodies['Mars'].name = 'Mars' #{"Mars"=>#<CelestiaBody:0x000000000553d4c8 @type="planet", @name="Mars">}
---
+++ Head_first/love_interest.rb
@@ -0,0 +1,12 @@
+class LoveInterest
+
+  def request_date
+    if @busy
+      puts "Sorry I'm busy."
+    else
+      puts "Sure, let's go!"
+      @busy = true
+    end
+  end
+  
+end
