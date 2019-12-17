b8d9626864fc8f0c41c6d53b59609041419a33ca
ST <staster86@gmail.com>
2019-12-17 18:12:54 +0400

    Комменты и создал новый файл, для изучения примисей(mixin).

--- Head_first/celestial_body.rb
+++ Head_first/celestial_body.rb
@@ -10,7 +10,7 @@ bodies = Hash.new do |hash, key| # = {}, 'Something'
   body = CelestiaBody.new # Передаём ссылку на объект
   body.type = 'planet' # Устанавливаем значение по умолчанию для параметра :type
   hash[key] = body # {'Mars' => #<CelestiaBody:0x000000000553d4c8 @type="planet">}
-  body
+  body # Возращаем значение
 end
 
 bodies['Mars'] # {'Mars' => #<CelestiaBody:0x000000000553d4c8 @type="planet">}
