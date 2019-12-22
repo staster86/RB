a6395444158ced650cd9311b545e847fc5a3ef18
ST <staster86@gmail.com>
2019-12-22 21:08:44 +0400

    YAML

--- Head_first/Movie/app.rb
+++ Head_first/Movie/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'movie'
+require 'yaml'
 
 get('/') do
   'Hello!'
--- Head_first/Movie/lib/movie.rb
+++ Head_first/Movie/lib/movie.rb
@@ -1,9 +1,12 @@
 class Movie
-  attr_accessor :title, :director, :year
 
-  def initialize(title: 'title', director: 'director', year: 'year')
+  attr_accessor :title, :director, :year, :id
+
+  def initialize(title: 'title', director: 'director', year: 'year', id: nil)
     self.title = title
     self.director = director
     self.year = year
+    self.id = id
   end
+
 end
